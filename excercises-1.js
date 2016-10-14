a9e256b10e1130e9c6f78d0f0032dfafbe8401ce
Carmen Chico <CarmenChico@icloud.com>
2016-10-14 16:40:53 -0400

    compeleted 1-5

--- excercises-1.js
+++ excercises-1.js
@@ -20,48 +20,149 @@
 ///////////////////////////////////////////////////////
 
 
-// Q1
+
+            //////////////////////
+            //////////////////////
+            ////              ////
+            ////      Q1      ////
+            ////              ////
+            //////////////////////
+            //////////////////////
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 var sum = function (num1, num2) {
-    return num1 + numb2;
+    return num1 + num2;
 };
 
+//another way to write the same thing
+////////////////////////////////////////
+//     function sum (num1, num2) {
+//         return num1 + num2;
+//     };
 
 
-// Q2
+
+              //////////////////////
+              //////////////////////
+              ////              ////
+              ////      Q2      ////
+              ////              ////
+              //////////////////////
+              //////////////////////
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
+//basic way to answer
+////////////////////////////////////////
+var avg = function (x, y, z) {
+
+  var total = x + y + z;
+  return total / 3;
+
+};
+
+
+//example of how to get an average from an array
+// function arrayAvg (nums) {
+//   var total = 0;
+//
+//   for (var i = 0; i < nums.length; i++) {
+//     var currentItem = nums[i];
+//     total = total + currentItem;
+//   }
+//
+//   return total / nums.length;
+//
+// }
+//
+// console.log(arrayAvg([3,7,1]));
 
 
 
-// Q3
+            //////////////////////
+            //////////////////////
+            ////              ////
+            ////    Q3        ////
+            ////              ////
+            //////////////////////
+            //////////////////////
+
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
+var getLength = function (aString) {
+  return aString.length;
+};
+
+
+//other way to write :
+////////////////////////////////////////
+//     function getLengh (aString) {
+//       return aString.length;
+//     };
 
 
 
-// Q4
+
+              //////////////////////
+              //////////////////////
+              ////              ////
+              ////      Q4      ////
+              ////              ////
+              //////////////////////
+              //////////////////////
+
 // Write a function called `greaterThan` that takes
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
 
+var greaterThan = function (nums1, nums2) {
+if (nums1>nums2)
+  return true;
+else{
+  return false;
+}
+};
 
 
 
-// Q5
+
+
+            //////////////////////
+            //////////////////////
+            ////              ////
+            ////      Q5      ////
+            ////              ////
+            //////////////////////
+            //////////////////////
+
 // Write a function called `greet` that takes a
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
+function greet (name) {
+  var name = ?
+  return "Hello, " + name;
+};
 
 
 
-// Q6
+// ////other way
+// var greet = function (param) {
+//
+//
+// };
+
+
+            //////////////////////
+            //////////////////////
+            ////              ////
+            ////      Q6      ////
+            ////              ////
+            //////////////////////
+            //////////////////////
 // Write a function called `madlib` that takes
 // 4 or more parameters (words). The function
 // should insert the words into a pre-defined
@@ -72,7 +173,10 @@ var sum = function (num1, num2) {
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 
+var madLib = function (a, b, c, d) {
 
+  return ;
+};
 
 
 ///////////////////////////////////////////////////////
